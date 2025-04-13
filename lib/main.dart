837a200f19bb9802f72da0b879ff3bf732236731
Tomas Ledesma <totem.ledesma@gmail.com>
2025-04-13 15:36:31 +0200

    firebase hosting ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:papa_dev/firebase_options.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(PapaDevApp());
 }
 
